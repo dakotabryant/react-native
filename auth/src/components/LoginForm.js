4b430bebba7b087f500c843db70e1229ed5b1889
Dakota Bryant <bryant.dakota@gmail.com>
2017-06-30 10:21:21 -0500

    More progress on mobile auth

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,22 +1,49 @@
 import React from 'react';
-import {TextInput} from 'react-native';
-import {Button, Card, CardSection} from './common';
+import firebase from 'firebase';
+import {TextInput, Text} from 'react-native';
+import {Button, Card, CardSection, Input} from './common';
 
 class LoginForm extends React.Component {
   state = {
-    text: ''
+    email: '',
+    password: '',
+    error: null
+  }
+  onButtonPress() {
+    const {email, password} = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .catch(() => {
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+      .catch(() => {
+        this.setState({error: 'Authentication Failed.'})
+      })
+    })
   }
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput value={this.state.text} onChangeText={(text) => this.setState({text})} style={{width: 100}}></TextInput>
+          <Input
+             placeholder='user@gmail.com'
+             label='Email'
+             value={this.state.text}
+             onChangeText={email => this.setState({email})}
+           />
         </CardSection>
         <CardSection>
-          <TextInput style={{width: 100}}></TextInput>
+          <Input
+            placeholder='password'
+            label='Password'
+            value={this.state.password}
+            onChangeText={password => this.setState({password})}
+            secureTextEntry
+          />
         </CardSection>
+        <Text>
+          {this.state.error}
+        </Text>
         <CardSection>
-          <Button>Log In</Button>
+          <Button onPress={this.onButtonPress.bind(this)}>Log In</Button>
         </CardSection>
       </Card>
     )
